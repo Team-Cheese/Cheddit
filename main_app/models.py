4f935baaa9293ec21864d4445c58cb5d44f0eebf
Bestnogm1 <koffiarielhessou@gmail.com>
2022-04-11 13:07:50 -0400

    add the about page

--- main_app/models.py
+++ main_app/models.py
@@ -2,3 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here
 
+import uuid 
+
+# Create your models here.
+class Channel(models.Model):
+  title = models.CharField('Channel Title', max_length=100, unique=True)
+  # created_by = models.ForeignKey(Profile)
+  description = models.TextField(max_length=250)
+  created = models.DateTimeField(auto_now_add=True)
+  id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+  def __str__(self):
+    return self.title
