fed4e57c03ed038222d33486c8afc38458a0df13
Bestnogm1 <koffiarielhessou@gmail.com>
2022-04-11 13:18:58 -0400

    imported channel in the views

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-
-
+from django.views.generic import ListView
+from .models import Channel
 def home(request):
   return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
+
+class Channel :
+  def __init__(self, title, description):
+    self.title = title
+    self.description = description
+
