8bf5244d0c4481ab326696cb43b5322cf6f31412
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-04-11 15:13:28 -0400

    Add login view

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
+  path('', views.Home.as_view(), name='home'),
   path('about/', views.about, name='about'),
   path('channels/', views.channels_index, name='channels_index'),
   path('channels/<init:channel_id/', views.channels_detail, name='channels_detail'),
