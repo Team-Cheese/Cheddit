5a94582570b0bfbc412c4ba5f718ce5091d25a1d
Caleb Vietinghoff <vietinghoffcaleb@gmail.com>
2022-04-11 11:12:53 -0400

    stub up first model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Channel(models.Model):
+  title = models.CharField(max_length=100)
+  description = models.TextField(max_length=250)
+  created = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return self.title
