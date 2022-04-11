802c4464bf9d6eb4c10813fc51f0f545c11443d2
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-04-11 15:36:15 -0400

    adding urls for signup

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
   path('about/', views.about, name='about'),
   path('channels/', views.channels_index, name='channels_index'),
   path('channels/<init:channel_id/', views.channels_detail, name='channels_detail'),
+  path('accounts/signup', views.signup, name='signup'),
 ]
