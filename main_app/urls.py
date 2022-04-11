9caaa563d2b58e50b0bfac62d0cde980c741ec05
Caleb Vietinghoff <vietinghoffcaleb@gmail.com>
2022-04-11 16:15:12 -0400

    work on debugging create channel form

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
   path('about/', views.about, name='about'),
   path('channels/', views.channels_index, name='channels_index'),
   path('channels/<str:channel_id>/', views.channels_details, name='channels_details'),
+  path('channels/create/', views.ChannelCreate.as_view(), name='channels_create')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView
 from .models import Channel
+
 def home(request):
   return render(request, 'home.html')
 
@@ -16,3 +17,7 @@ def channels_index(request) :
 def channels_details(request , channel_id) :
   channels = Channel.objects.get(id=channel_id)
   return render(request, 'channels/details.html', {'channels': channels})
+
+class ChannelCreate(CreateView):
+  model = Channel
+  fields = ['title', 'description']
