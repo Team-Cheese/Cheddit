71838cc97acd7e7afde770e5958a708835d83b10
Caleb Vietinghoff <vietinghoffcaleb@gmail.com>
2022-04-11 11:34:43 -0400

    change unique key in channel model

--- main_app/models.py
+++ main_app/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+import uuid 
 
 # Create your models here.
 class Channel(models.Model):
-  title = models.CharField(max_length=100)
+  title = models.CharField('Channel Title', max_length=100, unique=True)
+  # created_by = models.ForeignKey(Profile)
   description = models.TextField(max_length=250)
   created = models.DateTimeField(auto_now_add=True)
+  id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
   def __str__(self):
     return self.title
