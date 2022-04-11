5caabdc0da6301380edc11dc14f43cfc2ac35cd4
Bestnogm1 <koffiarielhessou@gmail.com>
2022-04-11 13:34:39 -0400

    adding the index

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView
 from .models import Channel
 def home(request):
   return render(request, 'home.html')
@@ -7,8 +8,11 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
-class Channel :
-  def __init__(self, title, description):
-    self.title = title
-    self.description = description
+class Channel(CreateView) :
+  model = Channel
+  fields = '__all__'
 
+
+def channel_index(request) :
+  channels = Channel.objects.all()
+  return render(request, 'channel/index.html', {'channel': channels})
