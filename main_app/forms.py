a154a208998ff2927edff2bf71bfa490c71de6d5
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-04-12 14:46:20 -0400

    Creating profile form

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,12 @@
 from django.forms import ModelForm
-from .models import Thread
+from .models import Thread, UserProfile
 
 class ThreadForm(ModelForm):
   class Meta:
     model = Thread
     fields = ['title', 'body']
+
+class UserProfileForm(ModelForm):
+  class Meta:
+    model = UserProfile
+    fields = ['screen_name']
