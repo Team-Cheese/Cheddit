4c587d254e18c0182c0b7f50d0e4ec257af21eef
Caleb Vietinghoff <vietinghoffcaleb@gmail.com>
2022-04-11 21:30:33 -0400

    refactor thread form

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView
+from django.shortcuts import render, redirect
+
+from main_app.forms import ThreadForm
 from .models import Channel, Thread
+from .forms import ThreadForm
 
 def home(request):
   return render(request, 'home.html')
@@ -18,7 +22,16 @@ def channels_index(request) :
 
 def channels_details(request , channel_id) :
   channels = Channel.objects.get(id=channel_id)
-  return render(request, 'channels/details.html', {'channels': channels})
+  thread_form = ThreadForm()
+  return render(request, 'channels/details.html', {'channels': channels, 'thread_form': thread_form})
+
+def thread_create(request, channel_id):
+  form = ThreadForm(request.POST)
+  if form.is_valid():
+    new_thread = form.save(commit=False)
+    new_thread.channel_id = channel_id
+    new_thread.save()
+  return redirect('/channels/', channel_id=channel_id)
 
 class ChannelCreate(CreateView):
   model = Channel
