dc0fa390262f9a6a9a06b28a62766b9821bae554
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-04-12 19:28:38 -0400

    Editing signup view function

--- main_app/views.py
+++ main_app/views.py
@@ -7,7 +7,7 @@ from django.views.generic.edit import CreateView
 from django.shortcuts import render, redirect
 from main_app.forms import ThreadForm
 from .models import Channel, Thread
-from .forms import ThreadForm
+from .forms import ThreadForm, UserProfileForm
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 
@@ -54,18 +54,13 @@ class ChannelDelete(DeleteView) :
 def signup(request):
   error_message = ''
   if request.method == 'POST':
-    # This is how to create a 'user' form object
-    # that includes the data from the browser
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-      # This will add the user to the database
-      user = form.save()
-      # This is how we log a user in
+    user_form = UserCreationForm(request.POST)
+    if user_form.is_valid():
+      user = user_form.save()
       login(request, user)
-      return redirect('cats_index')
+      return redirect('')
     else:
       error_message = 'Invalid sign up - try again'
-  # A bad POST or a GET request, so render signup.html with an empty form
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'signup.html', context)
